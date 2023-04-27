2cde7715f23f40b25001a15b43e6da01619efdc0
LopatkinDA <LopatkinDA@quarta.su>
2023-04-27 18:57:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class MainClass
+{
+    public static void Main(string[] args)
+    {
+        static void SortArray(in int[] inputArr, out int[] sorteddesc, out int[] sortedasc)
+        {
+            sortedasc = SortArrayAsc(inputArr);
+            sorteddesc = SortArrayDesc(inputArr);
+        }
+
+        static int[] SortArrayAsc(int[] intArr)
+        {
+            return intArr.OrderBy(x => x).ToArray();
+        }
+        static int[] SortArrayDesc(int[] intArr)
+        {
+            return intArr.OrderByDescending(x => x).ToArray();
+        }
+
+        SortArray(new int[] { 1, 2, 3, 10, 9 }, out int[] des, out int[] asc);
+
+        foreach (int i in des)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("-----------------------");
+        foreach (int i in asc)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
